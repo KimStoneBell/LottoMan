23f405643d184e3fa55145577a10e51f7b347285
jonghyu <jong0827@gmail.com>
2019-06-03 09:45:05 +0900

    레포시토리 작업 시작 -step1 연결부 시작

--- app/src/main/kotlin/com/stonebell/lottoman/presentation/lotto/serch/LottoSearchDBViewModel.kt
+++ app/src/main/kotlin/com/stonebell/lottoman/presentation/lotto/serch/LottoSearchDBViewModel.kt
@@ -2,16 +2,14 @@ package com.stonebell.lottoman.presentation.lotto.serch
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.google.firebase.database.*
-import com.stonebell.lottoman.info.LottoData
-import io.reactivex.Single
+import com.stonebell.lottoman.datasource.LottoRepository
+import com.stonebell.lottoman.domain.ILottoRepository
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.plusAssign
-import io.reactivex.schedulers.Schedulers
 
 class LottoSearchDBViewModel : ViewModel(){
 
-    private val database: FirebaseDatabase by lazy { FirebaseDatabase.getInstance() }
+    private val database: ILottoRepository by lazy { LottoRepository() }
 
     val lottoInfoText = MutableLiveData<String>()
     val targetLottoText = MutableLiveData<String>()
@@ -20,8 +18,8 @@ class LottoSearchDBViewModel : ViewModel(){
 
     fun searchLottoInfo(){
         targetLottoText.value?.let{
-            disposes += getLottoInfoToFirebase(it.toInt())
-                    .map { "Last Lotto No. : ${it.getValue(LottoData::class.java).toString()}" }
+            disposes += database.searchLottoInfo(it.toInt())
+                    .map {it.balls.toString()}
                     .subscribe ({
                         lottoInfoText.postValue(it)
                     },{})
@@ -29,39 +27,10 @@ class LottoSearchDBViewModel : ViewModel(){
     }
 
     fun searchLottoLastNumber(){
-        disposes += getLastLottoNum()
-                .map { it.getValue(Int::class.java).toString() }
+        disposes += database.searchLottoInfo(ILottoRepository.SEARCH_INDEX_LAST)
+                .map { "Last Lotto No. : $it"}
                 .subscribe ({lottoInfoText.postValue(it)
         },{})
-    }
-
-    private fun getLottoInfoToFirebase(targetNo: Int) : Single<DataSnapshot> {
-        return database.reference
-                .child("GAMES")
-                .child("NO_$targetNo")
-                .createObserver()
-                .subscribeOn(Schedulers.io())
-    }
-
-    private fun getLastLottoNum() : Single<DataSnapshot> {
-        return database.reference
-                .child("LAST_GAME_NO")
-                .createObserver()
-                .subscribeOn(Schedulers.io())
-    }
-
-    inline fun DatabaseReference.createObserver(): io.reactivex.Single<DataSnapshot> {
-        return Single.create { emitter ->
-            this.addListenerForSingleValueEvent(object : ValueEventListener {
-                override fun onCancelled(p0: DatabaseError) {
-                    emitter.onError(RuntimeException(p0.message))
-                }
-
-                override fun onDataChange(p0: DataSnapshot) {
-                    emitter.onSuccess(p0)
-                }
-            })
-        }
     }
 
     override fun onCleared() {
